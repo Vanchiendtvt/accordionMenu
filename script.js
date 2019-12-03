8ac8d0442fd9f86d57c7196809dc4c0f772e097a
Vanchiendtvt <56372756+Vanchiendtvt@users.noreply.github.com>
2019-12-03 19:47:08 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,29 +3,22 @@ var accordion = document.getElementsByClassName('accordion');
 for(var i = 0; i< accordion.length;i++){
     createMenuAC(accordion[i]);
 };
-
-function clikSub(element){
-    for(var j =1; j < element.childNodes.length; j = j+2 ){
-        element.childNodes[j].addEventListener('click',(event) =>{
-            event.stopPropagation();
-        });
-    }
-}
-
 function createMenuAC(element){
-    var mnAC = element.childNodes[3];
-    clikSub(mnAC);
-    element.addEventListener('click',function(event){
-        event.preventDefault();
-        if( mnAC.style.height == '0px'||mnAC.style.height == ''){
-            mnAC.style.height = '120px';
-            }
-         else { mnAC.style.height = '0px';}   
-    }); 
-    element.addEventListener('blur',(event)=>{
-        console.log('ham blur');
-        mnAC.style.height = '0px';
+    var menuAc = element.childNodes[3];
+    var accordionElement = element;
+    menuAc.addEventListener('click',(event) =>{
+        event.stopPropagation();
     });
+    window.addEventListener('click',function(event){
+        if(accordionElement == event.target.parentElement){
+            event.preventDefault();
+            if( menuAc.style.height == '0px'||menuAc.style.height == ''){
+                menuAc.style.height = '120px';
+                }
+             else { menuAc.style.height = '0px';}
+        }
+          else {menuAc.style.height = '0px';}
+    }); 
     
 };
 }
